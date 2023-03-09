d361c76fd8a5f6a177d5458f885bec2a2a5ee7c0
Angel David Velasco <82744167+AngelVelasco1@users.noreply.github.com>
2023-03-09 18:40:10 -0500

    feat: :sparkles: Problema 4
    Creamos tres objetos, cada uno de los cuales representa una persona. Cada objeto tiene dos propiedades: nombre y edad. )  Le pedimos al usuario que escriba su edad. El nombre también se solicita al usuario.  Luego, se encuentra la edad de la persona mayor usando la función Math.max(), que encuentra el valor máximo de los argumentos que se le pasan.  Para encontrar la persona mayor, se compara la edad de cada objeto de persona con la edad mayor utilizando un bloque if...else. Si la edad de un objeto de persona se corresponde con la edad mayor, entonces ese objeto de persona se asigna a la variable personaMayor.  Por último, se utiliza el método alert() para mostrar un mensaje en la pantalla del usuario que indica la edad de la persona mayor y su nombre.

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-let intensity = parseFloat(prompt("¿Cual es la intensidad de la corriente?"))
-let resistance = parseFloat(prompt("¿Cual es la resistencia de la corriente?"))
+let person1  = {
+    nombre: prompt("1. Escriba su nombre"),
+    edad: parseInt(prompt("Cual es tu edad"))
+} 
+let person2  = {
+    nombre: prompt("2. Escriba su nombre"),
+    edad: parseInt(prompt("Cual es tu edad"))} 
+let person3  = {
+    nombre: prompt("3. Escriba su nombre"),
+    edad: parseInt(prompt("Cual es tu edad"))} 
 
-let volt = intensity * resistance;
-alert("El voltaje del circuito es de: " + volt + " Voltios");
+let definirMayorNum =  Math.max(person1.edad, person2.edad, person3.edad);
+
+let personaMayor;
+
+if (definirMayorNum === person1.edad) {
+    personaMayor = person1;
+} else if (definirMayorNum === person2.edad) {
+    personaMayor = person2;
+
+} else {
+    personaMayor = person3;
+}
+
+alert("La persona mayor tiene: " + definirMayorNum + " y se llama " + personaMayor.nombre);
