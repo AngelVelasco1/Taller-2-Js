35e0e44bd9e8c0b5ba5bfa89bb4aede241bfb494
Angel Velasco <angelgg2020@outlook.com>
2023-03-09 09:30:59 -0500

    feat: :sparkles: Problema 1
    Definimos 3 variables que almacenan cada nota que nos provee el usuario por medio de promts. Luego una variable llamada "promedio"  almacena el promedio de las 3 notas. Por medio de una condicional, definimos si el valor del promedio es menor o igual a 3.9 le decimos al estudiante que estudie por medio de un alert. De lo contrario sera Becado

---
+++ script.js
@@ -0,0 +1,13 @@
+let nota1 = parseFloat(prompt("Ingrese nota 1"));
+let nota2 = parseFloat(prompt("Ingrese nota 2"));
+let nota3 = parseFloat(prompt("Ingrese nota 3"));
+
+let promedio = (nota1 + nota2 + nota3) / 3;
+
+if (promedio <= 3.9) {
+    alert ("Estudie");
+
+} else {
+    alert ("Becado")
+}
+
