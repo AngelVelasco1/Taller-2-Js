390f54a554140f23cbb997da46799d7bf67ebddb
Angel velasco <angelgg2020@outlook.com>
2023-03-10 09:15:49 -0500

    feat: :sparkles: Problema 7
    Le preguntamos al usuario su nombre, el precio del articulo y el cantidad de articulos. Por medio de un alert mostramos el nombre del usuario, lo que debera pagar y la cantidad de articulos

--- script.js
+++ script.js
@@ -1,32 +1,5 @@
-let = nameList = [];
-let = genderList = "";
-let = noteList = [];
+name_user = prompt("Digite su nombre");
+price_article = prompt("Cuanto vale el articulo");
+amount = prompt("Cuantos/as vas a llevar");
 
-let masculine = [];
-let femenine = [];
-
-
-
-do {
-    nameList.push(prompt("Escribe tu Nombre:"));
-    genderList = (prompt("Escribe tu Género (M - F)"));
-
-    if(genderList == "M" || genderList == "m") {
-        masculine.push(1);
-
-    } else if(genderList == "F" || genderList == "f") {
-        femenine.push(1);
-    }
-
-    noteList.push(parseFloat(prompt("Escribe tu Nota:")));
-
-
-} while(confirm("Quieres añadir mas estudiantes?"));
-
-notaMax = Math.max(...noteList);
-notaMin = Math.min(...noteList);
-
-nombreMax = noteList.indexOf(Math.max(...noteList));
-nombreMin = noteList.indexOf(Math.min(...noteList));
-
-alert(`La mayor nota es ${notaMax} y su nombre es ${nameList[nombreMax]} y la menor nota es ${notaMin} y su nombre es ${nameList[nombreMin]} y hay ${masculine.length} hombres y ${femenine.length} mujeres`);
+alert(`${name_user} debera pagar ${price_article} por ${amount} articulos`)
