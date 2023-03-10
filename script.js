a393ec814d7a2fe83c5903aa04f8d408c66dbf6a
Angel velasco <angelgg2020@outlook.com>
2023-03-10 08:59:13 -0500

    feat: :sparkles: Problema 6
    Definimos 3 variables principales tipo array para guardar los nombres, el genero y las notas de los estudiantes. A su vez 2 variables para almacenar el genero de las personas. Por medio de un ciclo do...while preguntamos su nombre y lo añadimos mediante un push.  Luego preguntamos su genero, por medio de una condicional definimos si la letra añadida es "M"o "m" añadimos un elemento al array masculine, en caso de ser "F" o "f" lo añadimos al array femenine.  Luego preguntara si quiere repetir y añadir mas estudiantes, devolviendose al codigo del do. Seguido definimos 2 variables para determinar la nota maxima y minima de la lista, por medio de los operadores Math.max para el mayor y Math.min para el menor. Usamos los (...) para separa los numeros. Luego creamos dos variables que almacenan la posicion de las notas. Usando de nuevo los operadores Math definimos la posicion mayor y menor. FInalmente mediante un alert imprimimos la nota maxima usando backticks y llamando a la variable notaMax, junto al nombre de esa persona,llamando a la variable nombreMax, pero mostrando su nameList. Igual para la nota minima y nombre minima.

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-let num1 = parseFloat(prompt("Escriba el primer numero"));
-let num2 = parseFloat(prompt("Escriba el segundo numero"));
+let = nameList = [];
+let = genderList = "";
+let = noteList = [];
 
-let suma = num1 + num2;
-let resta = num1 - num2;
+let masculine = [];
+let femenine = [];
 
-let producto = num1 * num2;
-let division = num1 / num2;
-if (num1 > num2) {
-    alert(("La suma de los numeros es: " )+ suma  + (" y la diferencia es: " + resta));
-} else {
-    alert(("El producto de los numeros es: " )+ producto  + (" y la division es: " + division))
-}
+
+
+do {
+    nameList.push(prompt("Escribe tu Nombre:"));
+    genderList = (prompt("Escribe tu Género (M - F)"));
+
+    if(genderList == "M" || genderList == "m") {
+        masculine.push(1);
+
+    } else if(genderList == "F" || genderList == "f") {
+        femenine.push(1);
+    }
+
+    noteList.push(parseFloat(prompt("Escribe tu Nota:")));
+
+
+} while(confirm("Quieres añadir mas estudiantes?"));
+
+notaMax = Math.max(...noteList);
+notaMin = Math.min(...noteList);
+
+nombreMax = noteList.indexOf(Math.max(...noteList));
+nombreMin = noteList.indexOf(Math.min(...noteList));
+
+alert(`La mayor nota es ${notaMax} y su nombre es ${nameList[nombreMax]} y la menor nota es ${notaMin} y su nombre es ${nameList[nombreMin]} y hay ${masculine.length} hombres y ${femenine.length} mujeres`);
