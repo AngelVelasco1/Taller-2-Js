7323677894c4e7cb1dcea5531e5c45b8a727c22a
Angel David Velasco <82744167+AngelVelasco1@users.noreply.github.com>
2023-03-11 10:24:22 -0500

    feat: :sparkles: Problema 9
    Definimos dos variables tipo array, para almacenar los nombres de las atletas y su marca de salto respectivamente. Luego por medio de un ciclo do..while le preguntamos el nombre del atleta y lo añadimos al array por medio del push. Igual para la marca de salto, con el while le preguntamos al usuario si quiere añadir mas atletas. Definimos una variable para determinar la mayor marca, usando el index de meterMark, junto a la funcion Math.max para determinar la mayor marca. Definimos otra variable para determinar si se hizo record, el cual evalua si la marca mayor (championAtlethe) fue mayor a 15.5. Por utlimo usando una condicional if, evaluamos si el campeon hizo record o no. En ambos casos mostramos usando un alert el nombre de la atleta campeona, cambiando solo el mensaje si hizo record o no dependiendo del valor de record.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let atlethesNames = [];
+let meterMark = [];
 
+do {
+    atlethesNames.push(prompt("Escriba su nombre"));
+    meterMark.push(parseFloat(prompt("Cual es tu marca de salto en metros?"))); 
+}
+    while(confirm("¿Vas a añadir mas atletas?")) {  
+  }
+
+let championAtlethe = meterMark.indexOf(Math.max(...meterMark));
+let record = (meterMark[championAtlethe] > 15.5);
+
+if (record) {
+    alert(`La atleta campeona es ${atlethesNames[championAtlethe]} con un salto de ${meterMark[championAtlethe]} metros superando el record🤑`);
+
+} else {
+    alert(`La atleta campeona es ${atlethesNames[championAtlethe]} pero no hizo record😖`);
+}
